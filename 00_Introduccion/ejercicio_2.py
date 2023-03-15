9d658258626cd82864084259691667a706f3bbb6
Rodrigo Barragan <banolu75@gmail.com>
2023-03-15 17:24:13 -0600

    update ejercicio_2

--- 00_Introduccion/ejercicio_2.py
+++ 00_Introduccion/ejercicio_2.py
@@ -1,4 +1,5 @@
 import re
 frase = "Tengo 2 hijos que tienen 15 y 11 años"
 patron = '[0-9]+' #Esta es una expresión regular
-re.findall(patron, frase)
+exp_final=re.findall(patron, frase)
+print(exp_final)
